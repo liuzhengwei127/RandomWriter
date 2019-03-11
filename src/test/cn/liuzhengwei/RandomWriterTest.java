fc32c2f2c900790d54d74c19eabc55ad3f6aebc3
liuzhengwei127 <756670011@qq.com>
2019-03-11 18:47:29 +0800

    Junit added

---
+++ src/test/cn/liuzhengwei/RandomWriterTest.java
@@ -0,0 +1,57 @@
+package test.cn.liuzhengwei; 
+
+import cn.liuzhengwei.RandomWriter;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* RandomWriter Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÈýÔÂ 11, 2019</pre> 
+* @version 1.0 
+*/ 
+public class RandomWriterTest { 
+
+@Before
+public void before() throws Exception {
+    RandomWriter testWriter = new RandomWriter();
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: randomWrite() 
+* 
+*/ 
+@Test
+public void testRandomWrite() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: readFile() 
+* 
+*/ 
+@Test
+public void testReadFile() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: write() 
+* 
+*/ 
+@Test
+public void testWrite() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
